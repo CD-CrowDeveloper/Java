1f4e75bf3282cc660cfe3c2c112471672870f49f
YerobeGG <zlifeinvader@gmail.com>
2024-08-01 16:01:57 +0100

    String, copilar y ejecutar

---
+++ basics.java
@@ -0,0 +1,10 @@
+// javac nombre.java - copila el codigo
+// java nombre - ejecuta el codigo
+
+public class basics {
+    public static void main(String[] args) {
+
+        String mystring = "Esto es una cadena de teto";
+        System.out.println(mystring);
+    }
+}
